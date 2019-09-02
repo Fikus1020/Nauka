45450e93efc2e1c59b115ab0ae098d2e9640f5eb
Fikus1020 <45180885+Fikus1020@users.noreply.github.com>
2019-09-03 00:17:06 +0200

    Update flappyB.js

--- flappyB.js
+++ flappyB.js
@@ -1,5 +1,30 @@
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
+
+let posY = 50;
+let gravity = 0.05;
+let velocity = 0;
+let jump = -4;
+
+let bird = new Image();
 let background = new Image();
 
+bird.src = "images/bird.png";
 background.src = "images/background.png";
+
+function draw() {
+  ctx.drawImage(background, 0, 0);
+  ctx.drawImage(bird, 50, posY);
+  velocity += gravity;
+
+  if (posY>background.height-bird.height || posY<0){
+    velocity = 0; //stop game
+  }
+  posY = posY + velocity;
+
+  requestAnimationFrame(draw);
+}
+
+document.addEventListener('keydown', function(){ velocity=velocity+jump})
+
+draw();
